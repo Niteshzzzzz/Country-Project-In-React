61ebfc21373666a4f40901b2fd5d5cb2a04cba73
Nitesh Kumar <niteshkumarnkzzzzz@gmail.com>
2024-10-19 01:12:50 +0530

    link modify

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
     <footer>
     <div class="f-cotainer">
         <div class="f-icons">
-            <Link><i class="fa-brands fa-square-facebook"></i></Link>
+            <Link to={'https://www.facebook.com/profile.php?id=100053252281640&mibextid=ZbWKwL'}><i class="fa-brands fa-square-facebook"></i></Link>
             <Link to={'https://github.com/Niteshzzzzz/Country-Project-In-React'}><i class="fa-brands fa-square-github"></i></Link>
-            <Link><i class="fa-brands fa-square-instagram" id="in"></i></Link>
-            <Link><i class="fa-brands fa-linkedin"></i></Link>
+            <Link to={'https://www.instagram.com/niteshkumarnkzzzzz/profilecard/?igsh=MWRzcDhzMnUzM3F5bQ=='}><i class="fa-brands fa-square-instagram" id="in"></i></Link>
+            <Link to={'https://www.linkedin.com/in/nitesh-kumar-904393259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}><i class="fa-brands fa-linkedin"></i></Link>
         </div>
         <div class="c-name">&copy; FindCountry Private Limited</div>
         <div class="f-links">
